321b2679707bac0ec44bca0638bd30ae0db66fb9
zzsanshi <zzsanshi@163.com>
2021-07-19 23:42:11 +0800

    解决构造二叉树问题

--- src/main/java/leecode热门100题/_105从前序与中序遍历序列构造二叉树.java
+++ src/main/java/leecode热门100题/_105从前序与中序遍历序列构造二叉树.java
@@ -1,25 +1,36 @@
 package leecode热门100题;
 
 public class _105从前序与中序遍历序列构造二叉树 {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    public static void main(String[] args) {
+        int[] pre = new int[]{1,2};
+        int[] in = new int[]{2,1};
 
+        buildTree(pre ,in);
+    }
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
         return build(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
     }
-    TreeNode build(int[] preorder, int l1,int r1,int[] inorder,int l2,int r2) {
+    static TreeNode  build(int[] preorder, int l1,int r1,int[] inorder,int l2,int r2) {
+        if (l1 > r1) {
+            return null;
+        }
         if(l1 == r1) {
             return new TreeNode(preorder[l1]);
         }
         int val = preorder[l1];
-        int size = 0;
+        int inx = 0;
         for (int i = l2; i <= r2; i++) {
-            if(inorder[i] != val) {
-                size++;
-            }else {
-                break;
+            if(inorder[i] == val) {
+               inx = i;
+               break;
             }
         }
-        TreeNode left = build(preorder,l1+1,l1+size,inorder,l1+2,r2);
-        TreeNode root = new TreeNode(preorder[l1]);
-
+        int size = inx - l2;
+        //左子树
+        TreeNode left = build(preorder,l1+1,l1+size,inorder,l2,inx-1);
+        //右子树
+        TreeNode right = build(preorder,l1+size+1,r1,inorder,inx +1,r2);
+        TreeNode root = new TreeNode(preorder[l1],left,right);
+        return root;
     }
 }
